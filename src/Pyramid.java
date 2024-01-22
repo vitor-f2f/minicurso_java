c95c539fd3258e44311cf60d3e6e1ef0e56acd8b
vitor-f2f <vitor_henrique001@hotmail.com>
2024-01-21 21:58:35 -0300

    Numbers pyramid

---
+++ src/Pyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual a dimensao da piramide?");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
